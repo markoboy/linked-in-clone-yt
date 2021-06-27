22831fc5580356124657ed3ee8881b6854021221
Athanasios Markou <thanasismarko@gmail.com>
2021-06-27 11:49:37 +0100

    Added typeorm and .env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import envConfig from './configs/env.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [envConfig()], cache: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: configService.get('db.sqlite.database'),
+        logging: true,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/env.config.ts
@@ -0,0 +1,59 @@
+import { resolve } from 'path';
+
+export interface IEnvHttp {
+  host: string;
+  port: number;
+}
+
+export interface IEnvDatabasePG {
+  host: string;
+  port: number;
+  user: string;
+  password: string;
+  database: string;
+}
+
+export interface IEnvDatabaseSqLite {
+  database: string;
+}
+
+export interface IEnvDatabase {
+  postgres: IEnvDatabasePG;
+  sqlite: IEnvDatabaseSqLite;
+}
+
+export interface IEnvConfig {
+  tempPath: string;
+  http: IEnvHttp;
+  db: IEnvDatabase;
+}
+
+export interface IEnvConfigOptions {
+  tempPath?: string;
+}
+
+function envConfig({ tempPath }: IEnvConfigOptions = {}): () => IEnvConfig {
+  const path = tempPath || resolve(process.cwd(), '.temp');
+
+  return () => ({
+    tempPath: path,
+    http: {
+      host: process.env.HOST,
+      port: +process.env.PORT,
+    },
+    db: {
+      postgres: {
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+      },
+      sqlite: {
+        database: resolve(path, process.env.SQLITE_DATABASE),
+      },
+    },
+  });
+}
+
+export default envConfig;
